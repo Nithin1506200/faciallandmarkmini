e5da9d7ebfd14260cebb2592e40b5cdf6e940f61
Nithin N <57832822+Nithin1506200@users.noreply.github.com>
2021-09-01 09:59:28 +0530

    update

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-
-tf.ready().then(() => {
+ const myapp= ()=>{
     let video = document.getElementById('video');
-    let model;
     let canvas =document.getElementById("canvas");
     
     let ctx=canvas.getContext("2d");
@@ -13,38 +11,45 @@ tf.ready().then(() => {
             video.srcObject=stream;
         });
     };
-    const detectFaces = async () => {
-            const predictions = await model.estimateFaces({
-                input: video
-            });
+   setupCam();
+   const detectFaces = async () => {
+        var predictions = await faceapi.detectAllFaces(video).withFaceLandmarks(useTinymodel=true);
+
             ctx.clearRect(0,0,canvas.width,canvas.height);
-    if(predictions.length>0){
+          
+
+ if(predictions.length>0){
         predictions.forEach((pred)=>{
             ctx.beginPath();
             ctx.lineWidth="4";
             ctx.strokeStyle= "red";
-            ctx.rect(
+      /*      ctx.rect(
                 pred.boundingBox.topLeft[0],
                 pred.boundingBox.topLeft[1],
                 pred.boundingBox.bottomRight[0]-pred.boundingBox.topLeft[0],
                 pred.boundingBox.bottomRight[1]-pred.boundingBox.topLeft[1] 
-            );
-            ctx.stroke();
+            ); */
+           // ctx.stroke();
            // console.log(ctx);
-            ctx.fillStyle="green";
-            pred.scaledMesh.forEach(mesh =>{
-                ctx.fillRect(mesh[0],mesh[1],3,3)
-            });
+            ctx.fillStyle="black";
+
+               // ctx.fillRect(pred.positions[kk])
+               pred.landmarks.positions.forEach(point => {
+                ctx.fillRect(point['x'],point['y'],6,5)
+               })
+        
+           /* pred.positions.forEach(mesh =>{
+               ctx.fillRect(mesh[x],mesh[y],3,3)
+               console.log(mesh)
+            });  */
         });
-    }
+    } 
     };
-    setupCam();
     video.addEventListener("loadeddata",async () => {
-         model = await faceLandmarksDetection.load(
-            faceLandmarksDetection.SupportedPackages.mediapipeFacemesh);
-        setInterval(detectFaces,50);
-    })
-
-});
-
-
+        await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
+        await faceapi.loadTinyFaceDetectorModel('/models')
+        await faceapi.loadFaceLandmarkModel('/models');
+        setInterval(detectFaces,0);
+    });
+ }
+ myapp();
